37e47a12841a6dda6a796cbce53dbe37e5e34c75
Anh Tuan Hoang <anhht@avadagroup.com>
2025-05-26 13:47:14 +0700

    Add wrangler configuration and update package.json scripts for Cloudflare integration

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,20 @@
 import type { NextConfig } from "next";
+import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  // skip strict mode
-  reactStrictMode: false,
-  env: {
-    googleAnalyticsId: process.env.NODE_ENV === "production" ? process.env.GA_MEASUREMENT_ID : "",
-  }
+	/* config options here */
+	// skip strict mode
+	reactStrictMode: false,
+	env: {
+		googleAnalyticsId:
+			process.env.NODE_ENV === "production"
+				? process.env.GA_MEASUREMENT_ID
+				: "",
+	},
 };
+
+if (process.env.NODE_ENV === "development") {
+	await setupDevPlatform();
+}
 
 export default nextConfig;
